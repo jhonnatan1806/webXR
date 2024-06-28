888a6ced183d442d8db4457e771bfc82fde516cd
Jhonnatan Espinoza Rojas <jhonnatan.espinoza.r@uni.pe>
2024-06-28 13:53:15 -0500

    deploy

--- static/main.js
+++ static/main.js
@@ -1,57 +1,55 @@
 import { loadGLTF } from './libs/loader.js';
-import { mockWithVideo } from './libs/camera-mock.js';
 const THREE = window.MINDAR.IMAGE.THREE;
 
 document.addEventListener('DOMContentLoaded', () => {
-	const startRaccoon = async () => {
-		mockWithVideo('/static/assets/mock-videos/musicband2.mp4');
+	const start = async () => {
+		const imageTargets = [
+			{
+				src: '/static/assets/targets/moneda5.mind',
+				model: '/static/assets/models/moneda5/scene.gltf',
+				scale: { x: 0.65, y: 0.65, z: 0.65 },
+                position: { x: 0, y: -0.5, z: 0 },
+			},
+			{
+				src: '/static/assets/targets/moneda50.mind',
+				model: '/static/assets/models/moneda50/scene.gltf',
+				scale: { x: 0.15, y: 0.15, z: 0.15 },
+                position: { x: 0, y: 0, z: 0 },
+			},
+            {
+				src: '/static/assets/targets/moneda10.mind',
+				model: '/static/assets/models/moneda10/scene.gltf',
+				scale: { x: 0.5, y: 0.5, z: 0.5 },
+                position: { x: 0, y: -0.25, z: 0 },
+			}
+		];
+        let count = 0;
+		for (let target of imageTargets) {
+			const mindarThree = new window.MINDAR.IMAGE.MindARThree({
+				container: document.body,
+				imageTargetSrc: target.src,
+			});
+			const { renderer, scene, camera } = mindarThree;
 
-		const mindarThree = new window.MINDAR.IMAGE.MindARThree({
-			container: document.body,
-			imageTargetSrc: '/static/assets/targets/musicband-raccoon.mind',
-		});
+			const light = new THREE.HemisphereLight(0xffffff, 0xbbbbff, 1);
+			scene.add(light);
 
-		const { renderer, scene, camera } = mindarThree;
+			const model = await loadGLTF(target.model);
 
-		const light = new THREE.HemisphereLight(0xffffff, 0xbbbbff, 1);
-		scene.add(light);
+			model.scene.scale.set(target.scale.x, target.scale.y, target.scale.z);
+			model.scene.position.set(target.position.x, target.position.y, target.position.z);
+            model.scene.rotation.set(0, 0, 0);
 
-		const raccoon = await loadGLTF('/static/assets/models/musicband-raccoon/scene.gltf');
-		raccoon.scene.scale.set(0.01, 0.01, 0.01);
-		raccoon.scene.position.set(0, -0.4, 0);
-		const anchor = mindarThree.addAnchor(0);
-		anchor.group.add(raccoon.scene);
+			const anchor = mindarThree.addAnchor(count);
+			anchor.group.add(model.scene);
 
-		await mindarThree.start();
-		renderer.setAnimationLoop(() => {
-			renderer.render(scene, camera);
-		});
+			await mindarThree.start();
+			renderer.setAnimationLoop(() => {
+				renderer.render(scene, camera);
+			});
+
+            count++;
+		}
 	};
-
-	const startBear = async () => {
-		mockWithVideo('/static/assets/mock-videos/musicband2.mp4');
-
-		const mindarThree2 = new window.MINDAR.IMAGE.MindARThree({
-			container: document.body,
-			imageTargetSrc: '/static/assets/targets/musicband-bear.mind',
-		});
-
-		const { renderer, scene, camera } = mindarThree2;
-
-		const light = new THREE.HemisphereLight(0xffffff, 0xbbbbff, 1);
-		scene.add(light);
-
-		const bear = await loadGLTF('/static/assets/models/musicband-bear/scene.gltf');
-		bear.scene.scale.set(0.01, 0.01, 0.01);
-		bear.scene.position.set(0, -0.4, 0);
-		const anchor2 = mindarThree2.addAnchor(0);
-		anchor2.group.add(bear.scene);
-
-		await mindarThree2.start();
-		renderer.setAnimationLoop(() => {
-			renderer.render(scene, camera);
-		});
-	};
-
-	Promise.all([startRaccoon(), startBear()]);
+	start();
 });
